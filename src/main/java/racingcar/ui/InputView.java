2d4a647ecf501d320675fd6ad99368b0a9279d01
Isaac Han <isaachan233@gmail.com>
2023-12-07 14:43:08 +0900

    feat: 차 입력, 시도할 회수 입력받는 함수 추가

--- src/main/java/racingcar/ui/InputView.java
+++ src/main/java/racingcar/ui/InputView.java
@@ -1,4 +1,15 @@
 package racingcar.ui;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    public static String inputCars() {
+        System.out.printf("경주할 자동차 이름을 입력하세요.(이름은 쉼표(%s) 기준으로 구분)%n", ",");
+        return Console.readLine();
+    }
+
+    public static String inputLaps() {
+        System.out.printf("시도할 회수는 몇회인가요?%n");
+        return Console.readLine();
+    }
 }
