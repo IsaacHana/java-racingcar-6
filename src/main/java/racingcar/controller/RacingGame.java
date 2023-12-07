37b3e924c5f204108924bb22ea5682a6d98084d8
Isaac Han <isaachan233@gmail.com>
2023-12-07 14:50:21 +0900

    feat: 기능 요구 사항과 같게Controller 작성

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -1,5 +1,50 @@
 package racingcar.controller;
 
+import racingcar.domain.Cars;
+import racingcar.domain.RacingLaps;
+import racingcar.domain.Winners;
+import racingcar.ui.InputView;
+import racingcar.ui.OutputView;
+
 public class RacingGame {
-    
+    private Cars cars;
+    private RacingLaps laps;
+    private Winners winners;
+
+    public void run() {
+        collectInfo();
+        race();
+        announceWinners();
+    }
+
+    private void collectInfo() {
+        collectCars();
+        collectLaps();
+    }
+
+    private void collectCars() {
+        cars = Cars.createCarsByNames(InputView.inputCars());
+    }
+
+    private void collectLaps() {
+        laps = RacingLaps.valueOf(InputView.inputLaps());
+    }
+
+    private void race() {
+        int initLaps = 0;
+
+        OutputView.printPrevRaceMessage();
+        while (laps.getLaps() != initLaps++) {
+            OutputView.printRaceStatus(cars);
+        }
+    }
+
+    private void calculateWinners() {
+        winners = Winners.calculateWinners(cars.getCars());
+    }
+
+    private void announceWinners() {
+        calculateWinners();
+        OutputView.printRaceWinners(winners);
+    }
 }
