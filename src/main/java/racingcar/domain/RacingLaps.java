4072afc24e3e3319fe7b4b11f4d7d7de42d3c93c
Isaac Han <isaachan233@gmail.com>
2023-12-07 14:50:47 +0900

    feat: String 형태의 인자를 받아도 숫자로 변환할 수 있는 메소드 추가

--- src/main/java/racingcar/domain/RacingLaps.java
+++ src/main/java/racingcar/domain/RacingLaps.java
@@ -10,4 +10,16 @@ public class RacingLaps {
     public static RacingLaps valueOf(int laps) {
         return new RacingLaps(laps);
     }
+
+    public static RacingLaps valueOf(String laps) {
+        try {
+            return valueOf(Integer.valueOf(laps));
+        } catch (NumberFormatException nfe) {
+            throw new IllegalArgumentException("Integer 로 표현할 수 없는 문자입니다.");
+        }
+    }
+
+    public int getLaps() {
+        return laps;
+    }
 }
